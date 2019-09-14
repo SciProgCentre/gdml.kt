1e088c9721f4be1bea0b9c1e2809c7b779f47223
Alexander Nozik <altavir@gmail.com>
2019-09-14 19:53:02 +0300

    update versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,25 +1,14 @@
-import com.jfrog.bintray.gradle.tasks.BintrayUploadTask
-import org.gradle.api.publish.maven.internal.artifact.FileBasedMavenArtifact
-import java.util.*
-
 plugins {
-    id("scientifik.mpp") version "0.1.4"
-    //id("scientifik.publish") version "0.1.4"
-    `maven-publish`
-    id("com.jfrog.bintray") version "1.8.4"
+    id("scientifik.mpp") version "0.1.7"
+    id("scientifik.publish") version "0.1.7"
 }
 
 group = "scientifik"
-version = "0.1.4-dev-1"
+version = "0.1.4-dev-2"
 
 scientifik {
-    serialization = true
+    withSerialization()
 }
-
-val githubProject by extra("gdml.kt")
-val bintrayRepo by extra("scientifik")
-val vcs by extra("https://github.com/mipt-npm/gdml.kt")
-
 
 repositories {
     maven("https://dl.bintray.com/pdvrieze/maven")
@@ -39,7 +28,7 @@ kotlin {
     sourceSets {
         val commonMain by getting {
             dependencies {
-                api("net.devrieze:xmlutil-serialization:0.11.1.2")
+                api("net.devrieze:xmlutil-serialization:0.12.0.0")
                 api(kotlin("reflect"))
             }
         }
@@ -50,57 +39,6 @@ kotlin {
             languageSettings.apply {
                 useExperimentalAnnotation("kotlinx.serialization.ImplicitReflectionSerializer")
             }
-        }
-    }
-}
-
-publishing {
-    repositories {
-        maven("https://bintray.com/mipt-npm/$bintrayRepo")
-    }
-}
-
-bintray {
-    user = project.findProperty("bintrayUser") as? String?
-    key = project.findProperty("bintrayApiKey") as? String?
-    publish = true
-    override = true
-
-    pkg.apply {
-        userOrg = "mipt-npm"
-        repo = if (project.version.toString().contains("dev")) {
-            "dev"
-        } else {
-            bintrayRepo
-        }
-        name = project.name
-        issueTrackerUrl = "$vcs/issues"
-        setLicenses("Apache-2.0")
-        vcsUrl = vcs
-        version.apply {
-            name = project.version.toString()
-            vcsTag = project.version.toString()
-            released = Date().toString()
-        }
-    }
-
-    //workaround bintray bug
-    setPublications(*project.extensions.findByType<PublishingExtension>()!!.publications.names.toTypedArray())
-}
-
-afterEvaluate {
-    tasks.withType<BintrayUploadTask>() {
-        doFirst {
-            publishing.publications.filterIsInstance<MavenPublication>()
-                .forEach { publication ->
-                    val moduleFile =
-                        buildDir.resolve("publications/${publication.name}/module.json")
-                    if (moduleFile.exists()) {
-                        publication.artifact(object : FileBasedMavenArtifact(moduleFile) {
-                            override fun getDefaultExtension() = "module"
-                        })
-                    }
-                }
         }
     }
 }
