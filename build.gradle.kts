41c1e8b6828ab5bbec045ed7da3c39f61dc14033
Alexander Nozik <altavir@gmail.com>
2020-09-29 21:43:30 +0300

    Plugin 0.6.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,12 @@
-
 plugins {
-    id("ru.mipt.npm.mpp") version "0.6.0-dev-3"
-    id("ru.mipt.npm.publish") version "0.6.0-dev-3"
+    val toolsVersion = "0.6.1-dev-1.4.20-M1"
+    id("ru.mipt.npm.project") version toolsVersion
+    id("ru.mipt.npm.mpp") version toolsVersion
+    id("ru.mipt.npm.publish") version toolsVersion
 }
 
 group = "ru.mipt.npm"
-version = "0.2.0"
+version = "0.2.1"
 
 kscience {
     useSerialization {
@@ -13,15 +14,20 @@ kscience {
     }
 }
 
-repositories{
+repositories {
     mavenLocal()
+}
+
+ksciencePublish {
+    githubProject = "gdml.kt"
+    bintrayRepo = "kscience"
 }
 
 val bintrayRepo by extra("kscience")
 val githubProject by extra("gdml.kt")
 
 kotlin {
-    js(IR) {
+    js {
         browser {
             testTask {
                 useKarma {
@@ -32,13 +38,13 @@ kotlin {
     }
 
     sourceSets {
-        commonMain{
+        commonMain {
             dependencies {
 //                api(kotlin("reflect"))
             }
         }
 
-        jvmMain{
+        jvmMain {
             dependencies {
                 api("com.fasterxml.woodstox:woodstox-core:5.0.3")
             }
