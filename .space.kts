9c9f19820e61ba423c61d738b5c9e5d24b22f90e
Alexander Nozik <altavir@gmail.com>
2021-02-22 21:26:35 +0300

    update space CI

--- .space.kts
+++ .space.kts
@@ -1,21 +1,13 @@
-/**
-* JetBrains Space Automation
-* This Kotlin-script file lets you automate build activities
-* For more info, see https://www.jetbrains.com/help/space/automation.html
-*/
+job("Build") {
+    container("openjdk:11.0.10-jdk-buster") {
+        shellScript {
+            interpreter = "/bin/bash"
 
-job("Build and run tests") {
-   container("openjdk:11"){
-      shellScript {
-         content = """
-            sudo apt install -y libappindicator1 fonts-liberation
-            wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-            sudo dpkg -i google-chrome*.deb
-         """.trimIndent()
-      }
-      kotlinScript { api ->
-         api.gradlew("build")
-      }
-
-   }
+            content = """
+                apt update
+                apt install -y libncurses5
+                ./gradlew --no-daemon build
+            """.trimIndent()
+        }
+    }
 }
