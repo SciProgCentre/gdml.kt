82e171bd6b2ed46988540fd015a9a2f0395b0509
Alexander Nozik <altavir@gmail.com>
2021-02-22 21:34:38 +0300

    update space CI

--- .space.kts
+++ .space.kts
@@ -4,8 +4,10 @@ job("Build") {
             interpreter = "/bin/bash"
 
             content = """
-                apt update
-                apt install -y libncurses5
+                sudo apt update
+                sudo apt install -y libappindicator1 fonts-liberation
+                wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+                sudo dpkg -i google-chrome*.deb
                 ./gradlew --no-daemon build
             """.trimIndent()
         }
