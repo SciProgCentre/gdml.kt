95227f6eb82c2f5a4ce9382b42fbe5f3225c1d00
Alexander Nozik <altavir@gmail.com>
2021-02-23 07:19:35 +0000

    CI bump

--- .space.kts
+++ .space.kts
@@ -1,15 +1,3 @@
 job("Build") {
-    container("openjdk:11.0.10-jdk-buster") {
-        shellScript {
-            interpreter = "/bin/bash"
-
-            content = """
-                apt update
-                apt install -y libappindicator1 fonts-liberation
-                wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-                dpkg -i google-chrome*.deb
-                ./gradlew --no-daemon build
-            """.trimIndent()
-        }
-    }
+    gradlew("mipt-npm.registry.jetbrains.space/p/sci/containers/ci-environment:1.0.0", "build)
 }
