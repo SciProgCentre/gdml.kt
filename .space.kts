a88a7fc2ec368747f15a23408d3c10a15f6f8be8
Alexander Nozik <altavir@gmail.com>
2021-02-23 09:07:33 +0000

    bump CI

--- .space.kts
+++ .space.kts
@@ -1,3 +1,8 @@
 job("Build") {
-    gradlew("mipt-npm.registry.jetbrains.space/p/sci/containers/ci-environment:1.0.1", "build")
+    container("mipt-npm.registry.jetbrains.space/p/sci/containers/ci-environment:1.0.1") {
+        shellScript {
+            interpreter = "/bin/bash"
+            content = "./gradlew --no-daemon build"
+        }
+    }
 }
